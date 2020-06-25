fda5476c78eb4f68122b994ebca91fee0d81b599
Gabriel Giménez García <ggimenez@Itteria.cat>
2020-06-25 10:01:25 +0200

    'pub'

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to training-projects!');
+    expect(page.getTitleText()).toEqual('practica-ggimenez2040 app is running!');
   });
 
   afterEach(async () => {
